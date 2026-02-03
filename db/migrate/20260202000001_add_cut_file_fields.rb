70c3a84457c9b40ce7d94f581a65009b9175af91
tipografiafb-crypto <tipografiafb@gmail.com>
2026-02-03 14:05:16 +0100

    03-02-1405

--- db/migrate/20260202000001_add_cut_file_fields.rb
+++ db/migrate/20260202000001_add_cut_file_fields.rb
@@ -1,7 +1,7 @@
 class AddCutFileFields < ActiveRecord::Migration[7.2]
   def change
-    add_column :products, :has_cut_file, :boolean, default: false
+    add_column :products, :has_cut_file, :boolean, default: false unless column_exists?(:products, :has_cut_file)
     # Assuming we want to store the local path of the cut file associated with a product
-    add_column :products, :cut_file_path, :string
+    add_column :products, :cut_file_path, :string unless column_exists?(:products, :cut_file_path)
   end
 end
--- db/migrate/20260202000002_add_dependent_fields_to_products.rb
+++ db/migrate/20260202000002_add_dependent_fields_to_products.rb
@@ -1,7 +1,7 @@
 class AddDependentFieldsToProducts < ActiveRecord::Migration[7.2]
   def change
-    add_column :products, :is_dependent, :boolean, default: false
-    add_column :products, :master_product_id, :integer
-    add_index :products, :master_product_id
+    add_column :products, :is_dependent, :boolean, default: false unless column_exists?(:products, :is_dependent)
+    add_column :products, :master_product_id, :integer unless column_exists?(:products, :master_product_id)
+    add_index :products, :master_product_id unless index_exists?(:products, :master_product_id)
   end
 end
